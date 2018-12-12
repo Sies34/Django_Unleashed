86786a012323bb6ee50b1139aefe8299d0909342
jkuep <jkuepers@hotmail.com>
2018-12-12 20:50:38 +0100

    bl. 41

--- suorganizer/organizer/models.py
+++ suorganizer/organizer/models.py
@@ -6,12 +6,8 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(
-        max_length=31, unique=True)
-    slug = models.SlugField(
-        max_length=31,
-        unique=True,
-        help_text='A label for URL config.')
+    name = models.CharField(max_length=31, unique=True)
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config.')
 
     class Meta:
         ordering = ['name']
@@ -21,15 +17,10 @@ class Tag(models.Model):
 
 
 class Startup(models.Model):
-    name = models.CharField(
-        max_length=31, db_index=True)
-    slug = models.SlugField(
-        max_length=31,
-        unique=True,
-        help_text='A label for URL config. ')
+    name = models.CharField(max_length=31, db_index=True)
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config. ')
     description = models.TextField()
-    founded_date = models.DateField(
-        'date founded')
+    founded_date = models.DateField('date founded')
     contact = models.EmailField()
     website = models.URLField(max_length=255)
     tags = models.ManyToManyField(Tag)
@@ -40,27 +31,6 @@ class Startup(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=63)
-    slug = models.SlugField(
-        max_length=63,
-        help_text='A label for URL config',
-        unique_for_month='pub_date')
-    text = models.TextField()
-    pub_date = models.DateField(
-        'date published',
-        auto_now_add=True)
-    tags = models.ManyToManyField(
-        Tag, related_name='blog_posts')
-    startups = models.ManyToManyField(
-        Startup, related_name='blog_post')
-
-    def __str__(self):
-        return "{} on {}".format(
-            self.title,
-            self.pub_date.strftime('%Y-%m-%d'))
 
 
 class NewsLink(models.Model):
